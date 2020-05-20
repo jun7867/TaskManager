223a69dc4432ecface9a994147983cb45152def1
VoidNova88 <63839351+VoidNova88@users.noreply.github.com>
2020-05-20 20:21:10 +0900

    ShowMenu 에서 버튼이 왜 안보일까요 ㅠㅠ

---
+++ group/management/oodp/Menu.java
@@ -0,0 +1,12 @@
+package group.management.oodp;
+
+import javax.swing.*;
+
+public class Menu extends JFrame {
+
+	public static void main(String[] args) {
+		ShowMenu show = new ShowMenu();
+		show.screen();
+	}
+
+}
