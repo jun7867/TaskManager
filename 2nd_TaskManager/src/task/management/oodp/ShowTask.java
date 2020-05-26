2bfe318603d40004dd9dd899c0bf9c4257ea3cca
남쭌 <jjune095@naver.com>
2020-05-27 01:37:43 +0900

    delete Task

--- 2nd_TaskManager/src/task/management/oodp/ShowTask.java
+++ 2nd_TaskManager/src/task/management/oodp/ShowTask.java
@@ -1,6 +1,7 @@
 package task.management.oodp;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -12,10 +13,12 @@ import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import group.management.oodp.GroupDTO;
@@ -30,9 +33,18 @@ public class ShowTask extends JFrame{
 	DefaultTableModel model;
 	Vector<String> rows;
 
-	public void ShowTask(UserDTO user,GroupDTO group) {
-		JPanel panel = new JPanel();
-		//frame=new JFrame("Show Task");
+	public void ShowTask(UserDTO user,GroupDTO group) {	
+//		JPanel panel = new JPanel();
+//		JPanel bottomPanel = new JPanel();
+//		bottomPanel.setLayout(new GridLayout(2,1));
+//		
+//		JTextField tfName=new JTextField(10);
+//		JTextField tfMember=new JTextField(10);
+//		JTextField tfStartday=new JTextField(8);
+//		JTextField tfEndday=new JTextField(8);
+//		panel.add(comp)
+		
+		
 		BufferedReader logbuff = null;
 		ArrayList<Task> taskList = new ArrayList<>();
 		try {
@@ -71,10 +83,38 @@ public class ShowTask extends JFrame{
 
 		model=new DefaultTableModel(colNames,0);
 		tableView=new JTable(model);
-		panel.setLayout(new BorderLayout());
 		scrollList=new JScrollPane(tableView);
-		panel.add(scrollList,BorderLayout.CENTER);
-
+		add(scrollList,BorderLayout.CENTER);
+		
+		
+		
+		JPanel panel = new JPanel();
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new GridLayout(2,1));
+		
+		JTextField tfName=new JTextField(10);
+		JTextField tfMember=new JTextField(10);
+		JTextField tfStartday=new JTextField(8);
+		JTextField tfEndday=new JTextField(8);
+		panel.add(new JLabel("Task"));
+		panel.add(tfName);
+		panel.add(new JLabel("Member"));
+		panel.add(tfMember);
+		panel.add(new JLabel("StartDay"));
+		panel.add(tfStartday);
+		panel.add(new JLabel("EndDay"));
+		panel.add(tfEndday);
+		bottomPanel.add(panel);
+		
+		JPanel panel2= new JPanel();
+		JButton btnMod=new JButton("수정");
+		JButton btnDel=new JButton("Delete");
+		JButton btnBack = new JButton("뒤로가기");
+		panel2.add(btnMod);
+		panel2.add(btnDel);
+		panel2.add(btnBack);
+		bottomPanel.add(panel2);
+		
 
 		//Vector<String> rows=new Vector<>();
 		
@@ -92,23 +132,47 @@ public class ShowTask extends JFrame{
 			rows.addElement(taskList.get(i).getStartDate()+'-'+taskList.get(i).getEndDate());
 			model.addRow(rows);
 		}
-
-
-		JButton j1 = new JButton("뒤로가기");
-		add(j1);
-		j1.setBounds(200, 380, 100, 30);
-
-		add(panel);
+		
+		
+		
+		
+//		j1.setBounds(200, 380, 100, 30);
+//		panel.add(j1);
+//		bottomPanel.add(panel);
+		add(bottomPanel,BorderLayout.SOUTH);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setSize(600,450);
+		
+		setSize(900,500);
 		setTitle("업무 확인 페이지");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-
+		
+		
+		btnMod.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//선택한 줄 (row)번호 알아내기
+				int rowIndex=tableView.getSelectedRow();
+				
+				
+			}
+		});
+		
+		btnDel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//선택한 줄 (row)번호 알아내기
+				int rowIndex=tableView.getSelectedRow();
+				//선택 안하고 누를 경우 
+				if(rowIndex==-1) dispose();
+				model.removeRow(rowIndex);
+				// 실제로 파일 삭제는 아직 구현안함. 디자인 패턴 우선 하겠음.
+			}
+		});
 
 		//뒤로가기
-		j1.addActionListener(new ActionListener() {
+		btnBack.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
