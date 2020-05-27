2002e136ec1a8d83216adef4d74519d1847219aa
suyoung3476 <48312024+suyoung3476@users.noreply.github.com>
2020-05-27 12:51:41 +0900

    Update Menu.java

--- 2nd_TaskManager/src/group/management/oodp/Menu.java
+++ 2nd_TaskManager/src/group/management/oodp/Menu.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import task.management.oodp.TaskMenu;
+import schedule.management.oodp.ScheduleMenu;
 import user.management.oodp.UserDTO;
 
 public class Menu {
@@ -13,6 +14,7 @@ public class Menu {
 		JFrame f = new JFrame();
 		f.setSize(400, 400);
 		f.setVisible(true);
+		f.setTitle(group.getName()+" Home");
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout());
 		
@@ -25,10 +27,12 @@ public class Menu {
 		
 		JButton j1 = new JButton("업무 관리");
 		buttonPanel.add(j1);
-		
 		//j1.setBounds(150, 330, 80, 30);
 		
-	
+		JButton j2 = new JButton("스케줄 관리");
+		buttonPanel.add(j2);
+		//j2.setBounds(250, 330, 80, 30);
+		
 
 		f.add(buttonPanel, BorderLayout.SOUTH);
 		buttonPanel.setVisible(true);
@@ -41,6 +45,17 @@ public class Menu {
 			public void actionPerformed(ActionEvent e) {
 				TaskMenu taskMenu=new TaskMenu();
 				taskMenu.screen(user, group);
+				
+			}
+			
+		});
+		
+		j2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ScheduleMenu sMenu=new ScheduleMenu();
+				sMenu.screen(user, group);
 				
 			}
 			
