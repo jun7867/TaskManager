c5d1e5e80c84870b3a78e259046e6d2305d996f9
suyoung3476 <48312024+suyoung3476@users.noreply.github.com>
2020-05-27 12:19:16 +0900

    Update Schedule.java

--- 2nd_TaskManager/src/schedule/management/oodp/Schedule.java
+++ 2nd_TaskManager/src/schedule/management/oodp/Schedule.java
@@ -1,1 +1,51 @@
+package schedule.management.oodp;
 
+import java.util.ArrayList;
+
+public class Schedule {
+	private String groupName;
+	private String sched;
+	private String date;
+	private String content;
+	private ArrayList<String> userList = new ArrayList<>();
+
+	public Schedule(String sched, String date, String content) {
+	
+		this.sched = sched;
+		this.date = date;
+		this.content = content;
+	}
+
+	public String getSchedule() {
+		return sched;
+	}
+
+	public void setSchedule(String sched) {
+		this.sched = sched;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setdate(String date) {
+		this.date = date;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public ArrayList<String> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(ArrayList<String> userList) {
+		this.userList = userList;
+	}
+	
+}
