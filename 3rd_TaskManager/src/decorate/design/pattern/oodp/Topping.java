bd3d042f27a2a88bc6029d001b579f6d054b9826
suyoung3476 <48312024+suyoung3476@users.noreply.github.com>
2020-06-10 02:53:20 +0900

    D to d

---
+++ 3rd_TaskManager/src/decorate/design/pattern/oodp/Topping.java
@@ -0,0 +1,12 @@
+package decorate.design.pattern.oodp;
+
+public abstract class Topping implements DecoName{
+	protected DecoName decoName;
+	
+	public Topping(DecoName decoName) {
+		this.decoName=decoName;
+	}
+	
+	@Override
+	public abstract String getName();
+}
