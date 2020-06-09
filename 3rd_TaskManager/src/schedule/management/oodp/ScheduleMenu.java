be36a62e407ceb612898e1821b80f6995f8b95c6
suyoung3476 <48312024+suyoung3476@users.noreply.github.com>
2020-06-09 22:40:16 +0900

    Remove j3 button

--- 3rd_TaskManager/src/schedule/management/oodp/ScheduleMenu.java
+++ 3rd_TaskManager/src/schedule/management/oodp/ScheduleMenu.java
@@ -1,7 +1,7 @@
+  
 package schedule.management.oodp;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,12 +17,11 @@ import user.management.oodp.UserDTO;
 
 public class ScheduleMenu {
 
-	public void screen(UserDTO user, Group group, Color color) {
+	public void screen(UserDTO user, Group group) {
 		JFrame f = new JFrame();
-		f.setSize(500, 500);
+		f.setSize(400, 400);
 		f.setVisible(true);
 		f.setTitle("Schedule Menu Page");
-		f.getContentPane().setBackground(color);
 		
 		JLabel welcome = new JLabel(user.getName()+"님, 현재 "+ group.getName() +" 스케줄 관리에 접속해있습니다.");
 		f.add(welcome, BorderLayout.CENTER);
@@ -31,12 +30,9 @@ public class ScheduleMenu {
 		buttonPanel.setLayout(new FlowLayout());	
 		
 		JButton j1 = new JButton("스케줄 생성하기");
-		JButton j2 = new JButton("스케줄 수정하기");
-		JButton j3 = new JButton("스케줄 리스트 보기 ");
+		JButton j2 = new JButton("스케줄 확인/수정 ");
 		buttonPanel.add(j1);
 		buttonPanel.add(j2);
-		buttonPanel.add(j3);
-		
 		
 		f.add(buttonPanel, BorderLayout.SOUTH);
 		buttonPanel.setVisible(true);
@@ -52,17 +48,8 @@ public class ScheduleMenu {
 			
 		});
 		
-		j2.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				EditSchedule editSched=new EditSchedule();
-				editSched.EditSchedule(user,group);
-			}
-			
-		});
 		
-		j3.addActionListener(new ActionListener() {
+		j2.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
