39e3768e469affb1e950c70f19e3627901c72e41
suyoung3476 <48312024+suyoung3476@users.noreply.github.com>
2020-06-24 00:50:11 +0900

    Visitor Pattern 적용

--- 3rd_TaskManager/src/visitor/design/pattern/oodp/Client.java
+++ 3rd_TaskManager/src/visitor/design/pattern/oodp/Client.java
@@ -1,20 +1,26 @@
 package visitor.design.pattern.oodp;
 
-import schedule.management.oodp.Schedule;
-import task.management.oodp.Task;
+import java.text.ParseException;
 import group.management.oodp.Group;
+import schedule.management.oodp.Schedule;
 
 public class Client {
 
 	public static void main(String[] args) {	
-		//int total = calculate();
-		//System.out.println(total);
 	}
-
+	
+	//그룹 멤버 인원 체크 연산
 	public static int calculate(Group group) {
 		Visitor visitor = new VisitorImpl();
 		int mem = visitor.visit(group);
 		return mem;
+	}
+	
+	//해당 스케줄이 오늘로부터 몇일 후인지 계산
+	public static int dayLeft(Schedule Sched) throws ParseException {
+		Visitor visitor = new VisitorImpl();
+		int rem_day = visitor.visit(Sched);
+		return rem_day;
 	}
 
 }
