67d081b77a09572e718483c34eb9355877af9e75
heejuShin <49302519+heejuShin@users.noreply.github.com>
2020-06-23 14:42:24 +0900

    Update Setting.java

--- 3rd_TaskManager/src/group/management/oodp/Setting.java
+++ 3rd_TaskManager/src/group/management/oodp/Setting.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
 
 import memento.design.pattern.oodp.CareTaker;
 import memento.design.pattern.oodp.Originator;
+import user.management.oodp.UserDTO;
 
 public class Setting extends JFrame {
 	private Font f1;
@@ -22,22 +23,25 @@ public class Setting extends JFrame {
 	CareTaker careTaker = new CareTaker();
 	
 	
-	public void screen() {
+	public void screen(UserDTO user) {
 		JPanel panel = new JPanel();
 		Label l1 = new Label("폰트 사이즈"); add(l1);
 		l1.setBounds(100, 50, 60, 30);
 		TextField t1 = new TextField(); add(t1);
 		t1.setBounds(180, 50, 150, 30);
 		
-		JButton j1 = new JButton("등록");
+		JButton j1 = new JButton("적용");
 		JButton j2 = new JButton("취소");
 		JButton j3 = new JButton("되돌리기");
+		JButton j4 = new JButton("확인");
 		add(j1);
 		add(j2);
 		add(j3);
+		add(j4);
 		j1.setBounds(80, 330, 80, 30);
 		j2.setBounds(200, 330, 80, 30);
 		j3.setBounds(320, 330, 80, 30);
+		j4.setBounds(350, 50, 80, 30);
 		add(panel);
 		setSize(500,400);
 		setTitle("환경 설정");
@@ -46,7 +50,7 @@ public class Setting extends JFrame {
 		originator.setFont(12); // 초기값 저장.
 		careTaker.add(originator.saveFontToMemento());
 		
-		j1.addActionListener(new ActionListener() {
+		j4.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -55,17 +59,29 @@ public class Setting extends JFrame {
 					careTaker.add(originator.saveFontToMemento());
 					
 					f1 = new Font("돋움",Font.PLAIN,Integer.parseInt(t1.getText()));
-					l1.setFont(f1);
+					setFont(f1);
+					//l1.setFont(f1);
 					System.out.println(t1.getText());
 				}catch(Exception ex) {
 						
 				}
 			}
 		});
+		j1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				ManageGroup menu = new ManageGroup();
+		        menu.screen(user, f1);
+			}
+		});
 		j2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
+				ManageGroup menu = new ManageGroup();
+				f1 = new Font("돋움",Font.PLAIN,12);
+		        menu.screen(user, f1);
 			}
 		});
 		
@@ -79,7 +95,8 @@ public class Setting extends JFrame {
 				originator.getFontFromMemento(careTaker.get(0));
 				System.out.println(originator.getFont());
 				f1 = new Font("돋움",Font.PLAIN,originator.getFont());
-				l1.setFont(f1);
+				setFont(f1);
+				//l1.setFont(f1);
 			}
 		});
 	}
