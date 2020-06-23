ba79b98c990427c1b802a7d13486dc9f343eda87
suyoung3476 <48312024+suyoung3476@users.noreply.github.com>
2020-06-24 00:48:58 +0900

    Visitor Pattern 적용

--- 3rd_TaskManager/src/visitor/design/pattern/oodp/VisitorImpl.java
+++ 3rd_TaskManager/src/visitor/design/pattern/oodp/VisitorImpl.java
@@ -6,23 +6,18 @@ import java.util.Calendar;
 import java.util.Date;
 
 import schedule.management.oodp.Schedule;
-import task.management.oodp.Task;
 import group.management.oodp.Group;
 
 public class VisitorImpl implements Visitor  {
 	
-	
+	//그룹 멤버 인원 체크 연산
 	@Override
 	public int visit(Group group) {
 		int number = group.getNum();
 		return number;
 	}
-	@Override
-	public int visit(Task task) {
-		int number=0;
-		return number;
-	}
-
+	
+	//해당 스케줄이 오늘로부터 몇일 후인지 계산
 	@Override
 	public int visit(Schedule sched) throws ParseException {
 		Calendar calendar = Calendar.getInstance(); 
@@ -30,13 +25,9 @@ public class VisitorImpl implements Visitor  {
 		String from = sched.getDate();
 		SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		Date Dday = transFormat.parse(from);
-		long diff = today.getTime() - Dday.getTime();
+		long diff = Dday.getTime() - today.getTime();
 		long remain_day = diff/(24*60*60*1000);
 		return (int)remain_day;
 	}
 	
-	//@Override
-	//public int visit(Meeting meeting) {
-	//}
-
 }
